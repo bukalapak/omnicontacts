4d7f3d2df893e8d314770e35ad0e1784040b2519
Rindra Wiska <wiska.rindra@gmail.com>
2017-07-13 19:08:31 +0700

    add importer

--- lib/omnicontacts/importer.rb
+++ lib/omnicontacts/importer.rb
@@ -2,7 +2,7 @@ module OmniContacts
   module Importer
 
     autoload :Gmail, "omnicontacts/importer/gmail"
-    autoload :Friendshipgmail "omnicontacts/importer/gmail"
+    autoload :Friendshipgmail, "omnicontacts/importer/gmail"
     autoload :Yahoo, "omnicontacts/importer/yahoo"
     autoload :Hotmail, "omnicontacts/importer/hotmail"
     autoload :Outlook, "omnicontacts/importer/outlook"
